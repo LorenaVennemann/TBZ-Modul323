f64a7ea1c5ab9f45b918fbd7b3c64725158ade95
LorenaVennemann <jil.vennemann@gmail.com>
2025-06-26 13:24:55 +0200

    Initial project setup with basic data models

--- MiniProjekt/todolist.scala
+++ MiniProjekt/todolist.scala
@@ -1,19 +1,12 @@
 import scala.annotation.tailrec
-import scala.io.StdIn.readLine
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
-// Data models
 sealed trait Category {
   def name: String
   def subcategories: List[Category]
-  def withSubcategories(newSubcategories: List[Category]): Category
 }
 
-case class SimpleCategory(name: String, subcategories: List[Category] = Nil) extends Category {
-  def withSubcategories(newSubcategories: List[Category]): Category = 
-    copy(subcategories = newSubcategories)
-}
+case class SimpleCategory(name: String, subcategories: List[Category] = Nil) extends Category
 
 case class TodoItem(
   id: Int,
