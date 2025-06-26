ea27daae8b608fb7ac1709322dab55b1fcb59805
LorenaVennemann <jil.vennemann@gmail.com>
2025-06-26 13:26:50 +0200

    Add basic category operations (add, delete, find)

--- MiniProjekt/todolist.scala
+++ MiniProjekt/todolist.scala
@@ -16,7 +16,31 @@ case class TodoItem(
   done: Boolean = false
 )
 
+
 object TodoApp extends App {
   type TodoList = List[TodoItem]
   type CategoryList = List[Category]
+
+  // Category operations
+  def addCategory(categories: CategoryList, name: String, parentName: Option[String] = None): CategoryList = 
+    parentName match {
+      case None =>
+        if (categories.exists(_.name == name)) categories
+        else categories :+ SimpleCategory(name)
+      case Some(parent) =>
+        categories.map {
+          case c: SimpleCategory if c.name == parent =>
+            c.copy(
+              subcategories = if (c.subcategories.exists(_.name == name)) c.subcategories 
+              else c.subcategories :+ SimpleCategory(name)
+            )
+          case c => c
+        }
+    }
+
+  def deleteCategory(categories: CategoryList, name: String): CategoryList = 
+    categories.filterNot(_.name == name)
+
+  def findCategory(categories: CategoryList, name: String): Option[Category] = 
+    categories.find(_.name.equalsIgnoreCase(name))
 }
